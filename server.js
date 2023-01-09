1a227c73365243bef660f1fe86fc1a0d0f603985
ManhCuongN <nmcuong.cpr@gmail.com>
2023-01-09 23:46:16 +0700

    add api_port 2

--- server.js
+++ server.js
@@ -64,6 +64,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+
 const port = process.env.PORT || 5000 || process.env.API_PORT;
 http.listen(port, () => {
   console.log("Server is running on port", port);
