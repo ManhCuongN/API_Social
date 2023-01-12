aa9e7930f539e96b51e1939b1d158ef0a27cb1cc
ManhCuongN <nmcuong.cpr@gmail.com>
2023-01-12 21:26:57 +0700

    change apikey

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-const port = process.env.PORT || 5000 ;
+const port = process.env.PORT || 3000 ;
 http.listen(port, () => {
   console.log("Server is running on port", port);
 });
